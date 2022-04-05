c763a079d4380cb9b1e592e4befa6e7bcb69db2c
Adamoolah2 <102717042+Adamoolah2@users.noreply.github.com>
2022-04-05 19:44:13 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -54,6 +54,8 @@ function SignOut(){
 }
 
 function ChatRoom(){
+  
+  const dummy = useRef()
   const messagesRef = firestore.collection('messages')
   const query = messagesRef.orderBy('createdAt').limit(25);
 
@@ -74,13 +76,17 @@ function ChatRoom(){
     })
 
     setFormValue('');
+    
+    dummy.current.scrollIntoView({behavior : 'smooth'})
   }
 
   return(
     <>
-    <div>
+    <main>
       {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
-    </div>
+
+      <div ref={dummy}></div>
+    </main>
 
     <form onSubmit={sendMessage}>
       <input value={formValue} onChange={(e) => setFormValue(e.target.value)}/>
